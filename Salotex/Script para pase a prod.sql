25fc7ee1707198a2bfd54370219379274bd4f394
Javier Caballero <javiercpy@icloud.com>
2026-04-05 19:58:30 -0300

    Update Script para pase a prod.sql

--- Salotex/Script para pase a prod.sql
+++ Salotex/Script para pase a prod.sql
@@ -30,6 +30,18 @@ ALTER TABLE cab_orden_produccion
     id_tamanho_receta
   )
 /
+f_dev_costo_promedio_compra
+/
+Pag 170
+/
+SELECT f_dev_costo_promedio_compra(
+    p_id_producto => 244
+  , p_id_unidad_medida => 7
+  , p_id_moneda => 1
+  , p_fecha => SYSDATE
+  , p_mensaje => 1)
+FROM dual;
+/
 --receta
 SELECT c.id_receta,
        c.pro_id_producto_fin,
@@ -45,20 +57,41 @@ FROM cab_receta_produccion c
 WHERE c.id_receta = 25
 /
 --insumos
-SELECT d.id_det_receta,
-       d.rec_id_receta,
-       d.pro_id_producto_mat,
-       d.uni_id_unidad_medida,
-       d.cantidad_requerida,
-       d.mermas_porcentaje,
-       d.estado,
-       (
-        SELECT p.tipo
-        FROM producto p
-        WHERE p.id_producto = d.pro_id_producto_mat
-       ) tipo_articulo
-FROM det_receta_produccion d
-WHERE d.rec_id_receta = 25
+SELECT x.id_det_receta,
+       x.rec_id_receta,
+       x.pro_id_producto_mat,
+       x.uni_id_unidad_medida,
+       x.cantidad_requerida,
+       x.mermas_porcentaje,
+       x.estado,
+       x.tipo_articulo,
+       x.costo_promedio,
+       (x.costo_promedio * x.cantidad_requerida) AS costo_insumo
+FROM (
+  SELECT d.id_det_receta,
+         d.rec_id_receta,
+         d.pro_id_producto_mat,
+         d.uni_id_unidad_medida,
+         d.cantidad_requerida,
+         d.mermas_porcentaje,
+         d.estado,
+         (
+          SELECT p.tipo
+          FROM producto p
+          WHERE p.id_producto = d.pro_id_producto_mat
+         ) tipo_articulo,
+         f_dev_costo_promedio_compra(
+             p_id_producto => d.pro_id_producto_mat
+           , p_id_unidad_medida => d.uni_id_unidad_medida
+           , p_id_moneda => 1
+           , p_fecha => c.fecha_registro
+           , p_mensaje => 0) costo_promedio
+  FROM cab_receta_produccion c
+      ,det_receta_produccion d
+  WHERE c.id_receta = 1
+  AND d.rec_id_receta = c.id_receta
+) x
+
 /
 --costos operativos
 SELECT o.id_det_receta_ope,
@@ -68,7 +101,7 @@ SELECT o.id_det_receta_ope,
        o.costo_estimado,
        o.estado
 FROM det_receta_produccion_ope o
-WHERE o.rec_id_receta = 25
+WHERE o.rec_id_receta = 1
 AND o.estado = 'ACTIVO'
 /
 --prestadores
